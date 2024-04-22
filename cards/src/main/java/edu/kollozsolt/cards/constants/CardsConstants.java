0e5376507bbefe2eb5e7e9e725015ebe6ec782f7
EXTERNAL Kollo Zsolt (CodeSpring, HOME/ENG4) <external.zsolt.kollo@bosch.com>
2024-04-22 21:31:53 +0300

    card/audit and constants

---
+++ cards/src/main/java/edu/kollozsolt/cards/constants/CardsConstants.java
@@ -0,0 +1,17 @@
+package edu.kollozsolt.cards.constants;
+
+public class CardsConstants {
+    private CardsConstants(){}
+
+    public static String CREDIT_CARD = "Credit card";
+    public static int NEW_CARD_LIMIT = 1_00_000;
+    public static String STATUS_201 = "201";
+    public static String MESSAGE_201 = "Account created successfully.";
+    public static String STATUS_200 = "200";
+    public static String MESSAGE_200 = "Request processed successfully.";
+    public static String STATUS_417 = "417";
+    public static String MESSAGE_417_UPDATE = "Update operation failed. Please try again or contact Dev team.";
+    public static String MESSAGE_417_DELETE = "Delete operation failed. Please try again or contact Dev team.";
+//    public static String STATUS_500 = "500";
+//    public static String MESSAGE_500 = "An error occurred. Please try again or contact Dev team.";
+}
