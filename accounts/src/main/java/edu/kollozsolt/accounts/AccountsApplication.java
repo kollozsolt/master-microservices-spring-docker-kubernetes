b043dfe6702253d7df9c1e9f9dfaf67f06a4bdd6
EXTERNAL Kollo Zsolt (CodeSpring, HOME/ENG4) <external.zsolt.kollo@bosch.com>
2024-04-22 17:03:39 +0300

    accounts/open api documentation

--- accounts/src/main/java/edu/kollozsolt/accounts/AccountsApplication.java
+++ accounts/src/main/java/edu/kollozsolt/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package edu.kollozsolt.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Kollo Zsolt",
+                        email = "kollo.zsolt@codespring.ro",
+                        url = "https://google.com"
+                ),
+                license = @License(
+                    name = "Apache 2.0",
+                    url = "https://google.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "https://google.com"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
