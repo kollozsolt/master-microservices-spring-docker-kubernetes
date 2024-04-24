f9b3d391095473c17d3cc21c51b26cbccb36180a
EXTERNAL Kollo Zsolt (CodeSpring, HOME/ENG4) <external.zsolt.kollo@bosch.com>
2024-04-24 21:01:43 +0300

    loans/audit

---
+++ loans/src/test/java/edu/kollozsolt/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kollozsolt.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
