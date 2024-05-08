5655442202eb7f9d1394a9b9898827d2990c3f7b
EXTERNAL Kollo Zsolt (CodeSpring, HOME/ENG4) <external.zsolt.kollo@bosch.com>
2024-05-08 19:32:11 +0300

    configserver

---
+++ configserver/src/test/java/edu/kollozsolt/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kollozsolt.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
