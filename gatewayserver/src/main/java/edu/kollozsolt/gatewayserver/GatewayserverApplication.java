31725a0ff61a7a135de421166e948c94c1f9948d
EXTERNAL Kollo Zsolt (CodeSpring, HOME/ENG4) <external.zsolt.kollo@bosch.com>
2024-06-01 16:34:37 +0300

    gatewayserver

---
+++ gatewayserver/src/main/java/edu/kollozsolt/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,39 @@
+package edu.kollozsolt.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator easyBankRouteConfig(RouteLocatorBuilder builder, RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/easyBank/accounts/**")
+                        .filters(f -> f.rewritePath("/easyBank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/easyBank/loans/**")
+                        .filters(f -> f.rewritePath("/easyBank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/easyBank/cards/**")
+                        .filters(f -> f.rewritePath("/easyBank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS"))
+                .build();
+    }
+}
